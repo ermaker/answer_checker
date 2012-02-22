5e672c81b44c4e2641454f6e19f844e64d53c53d
Minwoo Lee <ermaker@gmail.com>
2012-02-22 16:37:32 +0900

    Modify the font and do i18n

---
+++ app/assets/javascripts/fontface.js
@@ -0,0 +1,7 @@
+google.load( "webfont", "1" );
+google.setOnLoadCallback(function() {
+  WebFont.load({ custom: {
+    families: [ "NanumGothic" ],
+    urls: [ "http://fontface.kr/NanumGothic/css" ]
+  }});
+});
